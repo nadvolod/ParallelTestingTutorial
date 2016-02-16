c874072e782ed683ed37ae0dd3b5a6a5ad14cea6
nadvolod <nadvolod@gmail.com>
2016-02-15 21:31:55 -0500

    1st commit of the parallel testing
    1st commit with the first nonworking test

---
+++ ParallelLocal/ParallelLocal.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Firefox;
+
+namespace ParallelLocal
+{
+    [TestFixture]
+    public class ParallelLocal
+    {
+        [Test]
+        public void Test1()
+        {
+            var driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("http://www.qtptutorial.net/automation-practice");
+            driver.FindElementByXPath("//a[contains(text(),'Follow')]").Click();
+        }
+    }
+}
---
+++ ParallelTestingTutorial/Class1.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace LocalTesting
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void SimpleTest1()
+        {
+            var driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("http://www.qtptutorial.net/automation-practice");
+            driver.FindElement(By.XPath("//a[contains(text(),'Follow')]")).Click();
+        }
+
+    }
+}
