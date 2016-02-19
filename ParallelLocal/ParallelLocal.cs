9312bbd1daf6178f37b288f62e7729f94a189196
nadvolod <nadvolod@gmail.com>
2016-02-19 09:09:03 -0500

    Added a 2nd parallelizable test

--- ParallelLocal/ParallelLocal.cs
+++ ParallelLocal/ParallelLocal.cs
@@ -4,6 +4,7 @@ using OpenQA.Selenium.Firefox;
 namespace ParallelLocal
 {
     [TestFixture]
+    [Parallelizable]
     public class ParallelLocal
     {
         [Test]
@@ -15,6 +16,22 @@ namespace ParallelLocal
             driver.FindElementById("idExample").Click();
             var elementCheck = driver.FindElementByXPath("//p[contains(text(),'Button success')]").Displayed;
             Assert.IsTrue(elementCheck, "Element was not present");
+        }
+    }
+
+    [TestFixture]
+    [Parallelizable]
+    public class ParallelLocal2
+    {
+        [Test]
+        public void Test2()
+        {
+            var driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("http://www.qtptutorial.net/automation-practice");
+            driver.Manage().Window.Maximize();
+            driver.FindElementByClassName("buttonClassExample").Click();
+            var elementCheck = driver.FindElementByXPath("//p[contains(text(),'Button success')]").Displayed;
+            Assert.IsFalse(elementCheck, "Element was not present");
         }
     }
 }
