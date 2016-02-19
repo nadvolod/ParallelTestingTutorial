8b2bf4f1a9362988ba8d8004cdd7ce170279e408
nadvolod <nadvolod@gmail.com>
2016-02-19 08:06:45 -0500

    Working test
    The test is now working

--- ParallelLocal/ParallelLocal.cs
+++ ParallelLocal/ParallelLocal.cs
@@ -11,7 +11,10 @@ namespace ParallelLocal
         {
             var driver = new FirefoxDriver();
             driver.Navigate().GoToUrl("http://www.qtptutorial.net/automation-practice");
-            driver.FindElementByXPath("//a[contains(text(),'Follow')]").Click();
+            driver.Manage().Window.Maximize();
+            driver.FindElementById("idExample").Click();
+            var elementCheck = driver.FindElementByXPath("//p[contains(text(),'Button success')]").Displayed;
+            Assert.IsTrue(elementCheck, "Element was not present");
         }
     }
 }
