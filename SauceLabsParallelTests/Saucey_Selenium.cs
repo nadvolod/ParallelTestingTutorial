984d40fa7e80852a6668045b466bae09f12d7b3e
nadvolod <nadvolod@gmail.com>
2016-02-29 19:19:51 -0500

    Added sauce labs tests

---
+++ SauceLabsParallelTests/Saucey_Selenium.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+namespace SauceLabsParallelTests
+{
+    [TestFixture("chrome", "45", "Windows 7", "", "")]
+    [Parallelizable]
+    public class SauceNUnit_Test
+    {
+        private IWebDriver driver;
+        private String browser;
+        private String version;
+        private String os;
+        private String deviceName;
+        private String deviceOrientation;
+
+        public SauceNUnit_Test(String browser, String version, String os, String deviceName, String deviceOrientation)
+        {
+            this.browser = browser;
+            this.version = version;
+            this.os = os;
+            this.deviceName = deviceName;
+            this.deviceOrientation = deviceOrientation;
+        }
+
+        [SetUp]
+        public void Init()
+        {
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.SetCapability(CapabilityType.BrowserName, browser);
+            caps.SetCapability(CapabilityType.Version, version);
+            caps.SetCapability(CapabilityType.Platform, os);
+            caps.SetCapability("deviceName", deviceName);
+            caps.SetCapability("deviceOrientation", deviceOrientation);
+            caps.SetCapability("username", "YOUR SAUCE LABS NAME");
+            caps.SetCapability("accessKey", "YOUR USER KEY");
+            caps.SetCapability("name", TestContext.CurrentContext.Test.Name);
+
+            driver = new RemoteWebDriver(new Uri("http://ondemand.saucelabs.com:80/wd/hub"), caps,
+                TimeSpan.FromSeconds(600));
+
+        }
+
+        [Test]
+        public void UltimateQATest2()
+        {
+            driver.Navigate().GoToUrl("http://courses.ultimateqa.com");
+            Assert.IsTrue(true);
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            bool passed = true;
+            try
+            {
+                // Logs the result to Sauce Labs
+                ((IJavaScriptExecutor)driver).ExecuteScript("sauce:job-result=" + (passed ? "passed" : "failed"));
+            }
+            finally
+            {
+                // Terminates the remote webdriver session
+                driver.Quit();
+            }
+        }
+    }
+}
---
+++ SauceLabsParallelTests/Saucey_Selenium2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+namespace SauceLabsParallelTests
+{
+    [TestFixture("chrome", "39", "Windows 7", "", "")]
+    [Parallelizable]
+    public class SauceySelenium2
+    {
+        private IWebDriver driver;
+        private String browser;
+        private String version;
+        private String os;
+        private String deviceName;
+        private String deviceOrientation;
+
+        public SauceySelenium2(String browser, String version, String os, String deviceName, String deviceOrientation)
+        {
+            this.browser = browser;
+            this.version = version;
+            this.os = os;
+            this.deviceName = deviceName;
+            this.deviceOrientation = deviceOrientation;
+        }
+
+        [SetUp]
+        public void Init()
+        {
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.SetCapability(CapabilityType.BrowserName, browser);
+            caps.SetCapability(CapabilityType.Version, version);
+            caps.SetCapability(CapabilityType.Platform, os);
+            caps.SetCapability("deviceName", deviceName);
+            caps.SetCapability("deviceOrientation", deviceOrientation);
+            caps.SetCapability("username", "nikolay-advolodkin");
+            caps.SetCapability("accessKey", "efca9495-e859-429f-bf96-740fd233370c");
+            caps.SetCapability("name", TestContext.CurrentContext.Test.Name);
+
+            driver = new RemoteWebDriver(new Uri("http://ondemand.saucelabs.com:80/wd/hub"), caps,
+                TimeSpan.FromSeconds(600));
+
+        }
+
+        [Test]
+        public void UltimateQATest1()
+        {
+            driver.Navigate().GoToUrl("http://courses.ultimateqa.com");
+            Assert.IsTrue(true);
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            bool passed = true;
+            try
+            {
+                // Logs the result to Sauce Labs
+                ((IJavaScriptExecutor)driver).ExecuteScript("sauce:job-result=" + (passed ? "passed" : "failed"));
+            }
+            finally
+            {
+                // Terminates the remote webdriver session
+                driver.Quit();
+            }
+        }
+    }
+}
